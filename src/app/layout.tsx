d88485a005fe2df9d8f80d9921aeb9a68de1c231
Vaibhav Deshmukh <vaibhav_deshmukh_007@yahoo.co.in>
2026-06-30 15:51:23 +0530

    feat: add Google Ads tracking tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,20 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* Google Ads Tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-936767269"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-936767269');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground transition-colors duration-300">
         <ThemeProvider>
